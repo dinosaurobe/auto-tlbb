9f31223047d46e2fdd93307e78daadb1689c6a33
iNetworkX <teddyle91@gmail.com>
2023-11-13 23:30:02 +0000

    update

--- setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET NEWLINE=^& echo.
-FIND /C /I "tieudattai.org" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%>>%WINDIR%\System32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO 127.0.0.1 tieudattai.org>>%WINDIR%\System32\drivers\etc\hosts
